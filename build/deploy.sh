73948f169d674e544b4015e6e4a571021a624354
Anton Wolkov <7001904+phntom@users.noreply.github.com>
2022-12-03 21:16:40 +0200

    added docker hash to image, version: 0.1.36

--- build/deploy.sh
+++ build/deploy.sh
@@ -7,7 +7,7 @@ set -ex
 # cd ..
 VERSION=$(grep -oP "(?<=appVersion: )[0-9.]+\b" charts/chadbot/Chart.yaml)
 echo kubectl scale deployment -n chat chadbot --replicas 0 > build.bat
-echo docker build . -f build/Dockerfile -t phntom/chadbot:$VERSION >> build.bat
+echo docker build . --pull -f build/Dockerfile -t phntom/chadbot:$VERSION >> build.bat
 echo docker push phntom/chadbot:$VERSION >> build.bat
 echo helm upgrade -n chat chadbot charts/chadbot -f charts/chadbot/values.d/minthe.secret.yaml --atomic --debug >> build.bat
 
